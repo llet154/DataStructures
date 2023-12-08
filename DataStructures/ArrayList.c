56e53ba83ce29d726e3045cd6046b7718c92682c
김성준 <dno06102@naver.com>
2023-12-08 17:57:37 +0900

    자료구조공부

--- DataStructures/ArrayList.c
+++ DataStructures/ArrayList.c
@@ -3,39 +3,50 @@
 //
 //void ListInit(List* plist)
 //{
-//	(plist->numOfData) = 0;
-//	(plist->curPosition) = -1;
+//	plist->numOfData = 0;
+//	plist->curPosition = -1;
 //}
 //
 //void LInsert(List* plist, LData data)
 //{
 //	if (plist->numOfData > LIST_LEN)
 //	{
-//		puts("저장이 불가능합니다.");
+//		puts("저장이 불가합니다.\n");
 //		return;
 //	}
 //
 //	plist->arr[plist->numOfData] = data;
-//	(plist->numOfData)++;
+//	plist->numOfData += 1;
 //}
 //
-//int LFirst(List* plist, LData* pdata)
+//int LSum(List* plist, LData num)
+//{
+//	int sum = 0;
+//	int i;
+//	for (i = 0; i < num; i++)
+//	{
+//		sum += plist->arr[i];
+//	}
+//	return sum;
+//}
+//
+//int LFirst(List* plist, LData* data)
 //{
 //	if (plist->numOfData == 0)
+//	{
 //		return FALSE;
-//
-//	(plist->curPosition) = 0;
-//	*pdata = plist->arr[0];
+//	}
+//	plist->curPosition = 0;
+//	*data = plist->arr[0];
 //	return TRUE;
 //}
 //
-//int LNext(List* plist, LData* pdata)
+//int LNext(List* plist, LData* data)
 //{
-//	if (plist->curPosition >= (plist->numOfData) - 1)
+//	if ((plist->numOfData) - 1 <= plist->curPosition)
 //		return FALSE;
-//
-//	(plist->curPosition)++;
-//	*pdata = plist->arr[plist->curPosition];
+//	plist->curPosition += 1;
+//	*data = plist->arr[plist->curPosition];
 //	return TRUE;
 //}
 //
--- DataStructures/ListMain.c
+++ DataStructures/ListMain.c
@@ -10,31 +10,23 @@
 //	ListInit(&list);
 //
 //	//5개의 데이터 저장
-//	LInsert(&list, 11); LInsert(&list, 11);
-//	LInsert(&list, 22); LInsert(&list, 22);
-//	LInsert(&list, 33);
+//	LInsert(&list, 1); LInsert(&list, 2);
+//	LInsert(&list, 3); LInsert(&list, 4);
+//	LInsert(&list, 5); LInsert(&list, 6);
+//	LInsert(&list, 7); LInsert(&list, 8);
+//	LInsert(&list, 9);
 //
-//	//저장된 데이터의 전체 출력
-//	printf("현재 데이터의 수: %d\n", LCount(&list));
-//
+//	printf("%d\n", LSum(&list, LCount(&list)));
+//	 
+//	//2와 3의 배수 다 삭제
 //	if (LFirst(&list, &data))
 //	{
-//		printf("%d ", data);
-//
-//		while (LNext(&list, &data))
-//			printf("%d ", data);
-//	}
-//	printf("\n\n");
-//
-//	//숫자 22을 탐색하여 모두 삭제
-//	if (LFirst(&list, &data))
-//	{
-//		if (data == 22)
+//		if (data%2==0 || data%3==0)
 //			LRemove(&list);
 //
 //		while (LNext(&list, &data))
 //		{
-//			if (data == 22)
+//			if (data % 2 == 0 || data % 3 == 0)
 //				LRemove(&list);
 //		}
 //	}
